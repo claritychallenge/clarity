3e9ea81e48dd22a5ff71c507c7bdc0ffcea07a8a
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2022-10-21 15:26:33 +0100

    Add validation for csf = None

--- clarity/enhancer/nalr.py
+++ clarity/enhancer/nalr.py
@@ -135,7 +135,7 @@ class NALR:
         hl_interpf = scipy.interpolate.interp1d(cfs, hl)
         return hl_interpf(self.aud)
 
-    def build(self, HL, cfs):
+    def build(self, HL, cfs=None):
         """
         Args:
             HL: hearing thresholds at [250, 500, 1000, 2000, 4000, 6000] Hz
@@ -144,6 +144,9 @@ class NALR:
             NAL-R FIR filter
             delay
         """
+        if cfs is None:
+            cfs = [250, 500, 1000, 2000, 4000, 6000]
+
         HL = self.hl_interp(np.array(HL), np.array(cfs))
         mloss = np.max(HL)
         if mloss > 0:
