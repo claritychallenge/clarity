ed7b95a81b01b15b8339fc55bcb7fb9abbb130fe
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2022-11-09 16:41:50 +0000

    Adding unit test

--- tests/evaluator/hasqi/test_hasqi.py
+++ tests/evaluator/hasqi/test_hasqi.py
@@ -1,3 +1,19 @@
+import numpy as np
+import pytest
+
+from clarity.evaluator.hasqi import hasqi_v2
+
+
 def test_hasqi_v2() -> None:
-    """Test for hasqi_v2 metric"""
-    assert True
+    """Test for hasqi_v2 index"""
+    np.random.seed(0)
+    sr = 16000
+    x = np.random.uniform(-1, 1, sr * 10)
+    y = np.random.uniform(-1, 1, sr * 10)
+
+    hl = np.array([45, 45, 35, 45, 60, 65])
+    eq = 1
+    level1 = 65
+
+    score, _, _, _ = hasqi_v2(x, sr, y, sr, hl, eq, level1)
+    assert score == pytest.approx(0.0012190655936307408, rel=1e-4)
