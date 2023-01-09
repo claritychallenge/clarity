3c4e0b89011216363832c0a2104afa7c79fd43ff
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-01-09 14:46:09 +0000

    Hasqi PEP8 nomenclature
    Hasqi evaluator given the PEP8 renaming treatment, improved one function name from clarity.evaluator.haspi.eb sub-module.

--- clarity/evaluator/hasqi/__init__.py
+++ clarity/evaluator/hasqi/__init__.py
@@ -1,3 +1,3 @@
-from clarity.evaluator.hasqi.hasqi import hasqi_v2, hasqi_v2_be
+from clarity.evaluator.hasqi.hasqi import hasqi_v2, hasqi_v2_better_ear  # noqa F401
 
 __all__ = ["hasqi_v2", "hasqi_v2_be"]
