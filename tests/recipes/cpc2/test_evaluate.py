ae222e03a059577754a74a74144082bf7e52abbf
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-03-08 09:38:07 +0000

    refactored test_evaluate using parameterized tests

--- tests/recipes/cpc2/test_evaluate.py
+++ tests/recipes/cpc2/test_evaluate.py
@@ -14,99 +14,106 @@ from recipes.cpc2.baseline.evaluate import (
 )
 
 
-def test_rmse_score():
-    """Test the function rmse_score"""
-    some_array = np.array([1, 2, 3])
-    empty_array = np.array([])
-
-    assert rmse_score(some_array, some_array) == 0.0
-    assert rmse_score(np.array([0]), np.array([1])) == 1.0
-    assert np.isnan(rmse_score(np.array([]), np.array([])))
-
-    # Should raise a ValueError exception is the arrays are not the same size
-    with pytest.raises(Exception):
-        rmse_score(empty_array, some_array)
+@pytest.mark.parametrize(
+    "x, y, expected", [([1, 2, 3], [1, 2, 3], 0), ([0], [1], 1), ([1, 1, 1], [1], 0)]
+)
+def test_rmse_score_ok(x, y, expected):
+    """Test the function rmse_score valid inputs"""
+    assert rmse_score(np.array(x), np.array(y)) == pytest.approx(expected)
 
 
-def test_ncc_score():
-    """Test the function ncc_score"""
-
-    # Correlation with self should be 1
-    assert ncc_score(np.array([1, 2, 3]), np.array([1, 2, 3])) == pytest.approx(1.0)
-
-    # Correlation with self should be 1
-    assert ncc_score(np.array([1, -1]), np.array([-1, 1])) == pytest.approx(-1.0)
-
-    with pytest.raises(Exception):
-        # correlation requires at least 2 data points
-        ncc_score(np.array([0]), np.array([1]))
-
-    with pytest.raises(Exception):
-        # correlation requires at least 2 data points
-        ncc_score(np.array([]), np.array([]))
-
-    with pytest.raises(Exception):
-        # Requires same size arrays
-        ncc_score(np.array([1, 2]), np.array([1, 2, 3]))
+@pytest.mark.parametrize(
+    "x, y, expected", [([1, 2, 3], [2, 3], ValueError), ([], [], ValueError)]
+)
+def test_rmse_score_error(x, y, expected):
+    """Test the function rmse_score for invalid inputs"""
+    with pytest.raises(expected):
+        result = rmse_score(np.array(x), np.array(y))
+        if np.isnan(result):
+            raise ValueError
 
 
-def test_kt_score():
-    """Test the function kt_score"""
-
-    # Correlation with self should be 1
-    assert kt_score(np.array([1, 2, 3]), np.array([1, 2, 3])) == pytest.approx(1.0)
-
-    # Correlation with self should be 1
-    assert kt_score(np.array([1, -1]), np.array([-1, 1])) == pytest.approx(-1.0)
-
-    # Requires at least 2 data points
-    assert np.isnan(kt_score(np.array([0]), np.array([1])))
-    assert np.isnan(kt_score(np.array([]), np.array([])))
-
-    with pytest.raises(Exception):
-        # Requires same size arrays
-        kt_score(np.array([1, 2]), np.array([1, 2, 3]))
+@pytest.mark.parametrize(
+    "x, y, expected", [([1, 2, 3], [1, 2, 3], 1), ([1, -1], [-1, 1], -1)]
+)
+def test_ncc_score_ok(x, y, expected):
+    """Test the function ncc_score valid inputs"""
+    assert ncc_score(np.array(x), np.array(y)) == pytest.approx(expected)
 
 
-def test_std_err():
-    """Test the function std_err"""
-
-    # Correlation with self should be 1
-    assert std_err(np.array([1, 2, 3]), np.array([1, 2, 3])) == 0
-
-    # Correlation with self should be 1
-    assert std_err(np.array([1, -1]), np.array([-1, 1])) == pytest.approx(
-        math.sqrt(2.0)
-    )
-
-    # Should be 0 for sequences of length 1
-    assert std_err(np.array([0]), np.array([1])) == pytest.approx(0)
-
-    # Should be 0 for sequences with constant differences
-    assert std_err(np.array([1, 2, 3]), np.array([11, 12, 13])) == pytest.approx(0)
-
-    # Requires at least 1 data points
-    assert np.isnan(std_err(np.array([]), np.array([])))
-
-    with pytest.raises(Exception):
-        # Requires same size arrays
-        std_err(np.array([1, 2]), np.array([1, 2, 3]))
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [([1], [2], ValueError), ([], [], ValueError), ([1, 2, 3], [2, 3], ValueError)],
+)
+def test_ncc_score_error(x, y, expected):
+    """Test the function ncc_score for invalid inputs"""
+    with pytest.raises(expected):
+        ncc_score(np.array(x), np.array(y))
 
 
-def test_compute_scores():
-    """Test the function compute_scores"""
+@pytest.mark.parametrize(
+    "x, y, expected", [([1, 2, 3], [1, 2, 3], 1), ([1, -1], [-1, 1], -1)]
+)
+def test_kt_score_ok(x, y, expected):
+    """Test the function kt_score valid inputs"""
+    assert kt_score(np.array(x), np.array(y)) == pytest.approx(expected)
 
-    with pytest.raises(Exception):
-        # Requires same size arrays
-        compute_scores(np.array([1, 2]), np.array([1, 2, 3]))
 
-    with pytest.raises(Exception):
-        # Requires at least 2 data points
-        compute_scores(np.array([1]), np.array([1]))
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [([1], [2], ValueError), ([], [], ValueError), ([1, 2, 3], [2, 3], ValueError)],
+)
+def test_kt_score_error(x, y, expected):
+    """Test the function kt_score for invalid inputs"""
+    with pytest.raises(expected):
+        result = kt_score(np.array(x), np.array(y))
+        if np.isnan(result):
+            raise ValueError
 
-    x = np.array([1, 2, 3])
-    result = compute_scores(x, x)
-    assert result["RMSE"] == rmse_score(x, x)
-    assert result["NCC"] == ncc_score(x, x)
-    assert result["KT"] == kt_score(x, x)
-    assert result["Std"] == std_err(x, x)
+
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [
+        ([1, 2, 3], [1, 2, 3], 0),
+        ([1, -1], [-1, 1], math.sqrt(2.0)),
+        ([1], [2], 0),
+        ([1, 2, 3], [11, 12, 13], 0),
+    ],
+)
+def test_std_err_ok(x, y, expected):
+    """Test the function std_err valid inputs"""
+    assert std_err(np.array(x), np.array(y)) == pytest.approx(expected)
+
+
+@pytest.mark.parametrize(
+    "x, y, expected", [([], [], ValueError), ([1, 2, 3], [2, 3], ValueError)]
+)
+def test_std_err_error(x, y, expected):
+    """Test the function std_err for invalid inputs"""
+    with pytest.raises(expected):
+        result = std_err(np.array(x), np.array(y))
+        if np.isnan(result):
+            raise ValueError
+
+
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [([], [], ValueError), ([1, 2, 3], [2, 3], ValueError), ([1], [2], ValueError)],
+)
+def test_compute_scores_error(x, y, expected):
+    """Test the function compute_scores for invalid inputs"""
+    with pytest.raises(expected):
+        compute_scores(np.array(x), np.array(y))
+
+
+@pytest.mark.parametrize("x, y", [([1, 2, 3], [1, 2, 3]), ([1, 2], [1, 3])])
+def test_compute_scores_ok(x, y):
+    """Test the function compute_scores for valid inputs"""
+    x = np.array(x)
+    y = np.array(y)
+    result = compute_scores(x, y)
+    assert len(result) == 4  # RMSE, NCC, KT, Std and no others
+    assert result["RMSE"] == rmse_score(x, y)
+    assert result["NCC"] == ncc_score(x, y)
+    assert result["KT"] == kt_score(x, y)
+    assert result["Std"] == std_err(x, y)
