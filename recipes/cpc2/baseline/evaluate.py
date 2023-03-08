0c7526858b8240f870a9d516c2c47ddad65f7027
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-03-08 00:10:00 +0000

    Started adding tests for CPC2 recipes

--- recipes/cpc2/baseline/evaluate.py
+++ recipes/cpc2/baseline/evaluate.py
@@ -12,27 +12,27 @@ from scipy.stats import kendalltau, pearsonr
 logger = logging.getLogger(__name__)
 
 
-def rmse_score(x, y):
+def rmse_score(x: np.ndarray, y: np.ndarray) -> float:
     """Compute the root mean squared error between two arrays"""
     return np.sqrt(np.mean((x - y) ** 2))
 
 
-def ncc_score(x, y):
+def ncc_score(x: np.ndarray, y: np.ndarray) -> float:
     """Compute the normalized cross correlation between two arrays"""
     return pearsonr(x, y)[0]
 
 
-def kt_score(x, y):
+def kt_score(x: np.ndarray, y: np.ndarray) -> float:
     """Compute the Kendall's tau correlation between two arrays"""
     return kendalltau(x, y)[0]
 
 
-def std_err(x, y):
+def std_err(x: np.ndarray, y: np.ndarray) -> float:
     """Compute the standard error between two arrays"""
     return np.std(x - y) / np.sqrt(len(x))
 
 
-def compute_scores(predictions, labels):
+def compute_scores(predictions, labels) -> dict:
     """Compute the scores for the predictions"""
     return {
         "RMSE": rmse_score(predictions, labels),
