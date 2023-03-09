bd6ebdc4729545ba6195d522a9ce055636e98a88
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2023-03-09 16:34:27 +0000

    set a gain of -5 dB LUFS as enhancement
    Signed-off-by: Gerardo Roa Dabike <gerardo.roa@gmail.com>

--- recipes/cad1/task2/baseline/enhance.py
+++ recipes/cad1/task2/baseline/enhance.py
@@ -2,61 +2,34 @@
 # pylint: disable=too-many-locals
 # pylint: disable=import-error
 
-import json
 import logging
-import warnings
 from pathlib import Path
 from typing import Tuple
 
 import hydra
-import librosa
 import numpy as np
-import pandas as pd
+import pyloudnorm as pyln
+from baseline_utils import read_mp3
+from evaluate import load_listeners_and_scenes
 from omegaconf import DictConfig
 from scipy.io import wavfile
 from tqdm import tqdm
 
-warnings.filterwarnings(
-    "ignore", message="PySoundFile failed. Trying audioread instead."
-)
-
-
 logger = logging.getLogger(__name__)
 
 
-def read_mp3(file_path: str) -> Tuple[np.ndarray, int]:
-    """Read an mp3 file and return its signal.
-
-    Args:
-     file_path (str): The path to the mp3 file.
-
-    Returns:
-        signal (np.ndarray): The signal of the mp3 file.
-        sample_rate (int): The sampling frequency of the mp3 file.
-    """
-    if not isinstance(file_path, str):
-        raise TypeError("Parameter song_path must be a string")
-
-    try:
-        signal, sample_rate = librosa.load(
-            file_path, sr=None, mono=False, res_type="kaiser_fast", dtype=np.float32
-        )
-    except Exception as error:
-        raise ValueError from error
-
-    return signal, sample_rate
-
-
-def enhance_song(waveform: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+def enhance_song(
+    waveform: np.ndarray, sample_rate: int, gain_db: float = 0.0
+) -> Tuple[np.ndarray, np.ndarray]:
     """
     Enhance a single song for a listener.
 
-    Baseline enhancement returns the original signal.
+    Baseline enhancement returns the original signal with a gain in dB LUFS.
 
     Args:
         waveform (np.ndarray): The waveform of the song.
-        listener_audiograms (dict): The audiograms of the listener.
-        cfg (DictConfig): The configuration file.
+        sample_rate (int): The sample rate of the song.
+        gain_db (float): The gain to apply to the song in dB LUFS. Defaults to 0.0.
 
     Returns:
         out_left (np.ndarray): The enhanced left channel.
@@ -85,8 +58,15 @@ def enhance_song(waveform: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     >> out_l, out_r = enhance_song(song_waveform, listener_audiograms, config)
 
     """
+
     if waveform.ndim == 1:
         waveform = np.array([waveform, waveform])
+
+    meter = pyln.Meter(sample_rate)
+    original_loudness = meter.integrated_loudness(waveform.T)
+    waveform = pyln.normalize.loudness(
+        waveform.T, original_loudness, original_loudness + gain_db
+    ).T
 
     out_left = waveform[0, :]
     out_right = waveform[1, :]
@@ -103,24 +83,13 @@ def enhance(config: DictConfig) -> None:
     Args:
         config (dict): Dictionary of configuration options for enhancing music.
     """
-    enhanced_folder_path = Path(config.path.enhanced_folder)
-    enhanced_folder_path.mkdir(parents=True, exist_ok=True)
+    enhanced_folder = Path("enhanced_signals") / config.evaluate.split
+    enhanced_folder.mkdir(parents=True, exist_ok=True)
 
-    # Load songs and listeners
-    with open(config.path.listeners_file, "r", encoding="utf-8") as fp:
-        listener_audiograms = json.load(fp)
+    # Load scenes and listeners depending on config.evaluate.split
+    scenes, listener_audiograms = load_listeners_and_scenes(config)
 
-    with open(config.path.scenes_file, "r", encoding="utf-8") as fp:
-        scenes = pd.read_json(fp, orient="index")
-        # You can load the train and validation scenes separately
-        # In the baseline we are processing the validation set directly
-        # and not training any system.
-        #
-        # Load train scenes as:
-        # train_scenes = scenes[scenes["split"] == "train"].to_dict("index")
-        valid_scenes = scenes[scenes["split"] == "valid"].to_dict("index")
-
-    for _, current_scene in tqdm(valid_scenes.items()):
+    for _, current_scene in tqdm(scenes.items()):
         song_id = current_scene["song"]
         song_path = (
             Path(config.path.music_dir)
@@ -131,8 +100,8 @@ def enhance(config: DictConfig) -> None:
         listener = listener_audiograms[listener_id]
 
         # Read song
-        song_waveform, sample_rate = read_mp3(song_path.as_posix())
-        out_l, out_r = enhance_song(song_waveform)
+        song_waveform, sample_rate = read_mp3(song_path.as_posix(), config.sample_rate)
+        out_l, out_r = enhance_song(song_waveform, sample_rate, gain_db=-5)
 
         enhanced = np.stack([out_l, out_r], axis=1)
         filename = f"{listener['name']}_{song_id}.wav"
@@ -145,7 +114,7 @@ def enhance(config: DictConfig) -> None:
             logger.warning(f"Writing {filename}: {n_clipped} samples clipped")
         np.clip(enhanced, -1.0, 1.0, out=enhanced)
         signal_16 = (32768.0 * enhanced).astype(np.int16)
-        wavfile.write(enhanced_folder_path / filename, sample_rate, signal_16)
+        wavfile.write(enhanced_folder / filename, sample_rate, signal_16)
 
 
 # pylint: disable = no-value-for-parameter
