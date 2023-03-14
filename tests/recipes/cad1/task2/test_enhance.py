c1cb6f5c5814829e966ce49302c1a882c35e557d
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2023-03-14 15:37:40 +0000

    update test_enhance.py test
    Signed-off-by: Gerardo Roa Dabike <gerardo.roa@gmail.com>

--- tests/recipes/cad1/task2/test_enhance.py
+++ tests/recipes/cad1/task2/test_enhance.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import numpy as np
 import pyloudnorm as pyln
+from omegaconf import DictConfig
 
 from recipes.cad1.task2.baseline.enhance import enhance_song
 
@@ -17,15 +18,25 @@ def test_enhance_song():
     np.random.seed(42)
 
     # Set the sample rate and gain
-    sample_rate = 16000
-    duration = 1.0
-    gain_db = 3.0
+    duration = 0.5
+
+    config = DictConfig(
+        {
+            "sample_rate": 16000,
+            "enhance": {"min_level": -11, "max_level": -19, "average_level": -14},
+        }
+    )
+    listener = {
+        "audiogram_levels_l": [20, 30, 35, 45, 50, 60, 65, 60],
+        "audiogram_levels_r": [20, 30, 35, 45, 50, 60, 65, 60],
+        "audiogram_cfs": [250, 500, 1000, 2000, 3000, 4000, 6000, 8000],
+    }
 
     # Create a sample waveform
-    waveform = np.random.rand(2, int(sample_rate * duration))
+    waveform = np.random.rand(2, int(config.sample_rate * duration))
 
     # Call the function
-    out_left, out_right = enhance_song(waveform, sample_rate, gain_db)
+    out_left, out_right = enhance_song(waveform, listener, config)
 
     expected_left = np.load(
         RESOURCES / "test_enhance.enhance_song_left.npy", allow_pickle=True
@@ -39,7 +50,7 @@ def test_enhance_song():
     np.testing.assert_array_almost_equal(out_right, expected_right)
 
     # Check that the output has the correct loudness
-    meter = pyln.Meter(sample_rate)
-    expected_loudness = meter.integrated_loudness(waveform.T) + gain_db
+    meter = pyln.Meter(config.sample_rate)
+
     out_loudness = meter.integrated_loudness(np.array([out_left, out_right]).T)
-    assert np.isclose(out_loudness, expected_loudness, atol=0.1)
+    assert np.isclose(out_loudness, -11, atol=0.1)
