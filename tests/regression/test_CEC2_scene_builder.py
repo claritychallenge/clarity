4852633d4af38a3a3c827042968872cae8e251ee
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-03-18 16:46:36 +0000

    Fixed files in tests/

--- tests/regression/test_CEC2_scene_builder.py
+++ tests/regression/test_CEC2_scene_builder.py
@@ -2,7 +2,7 @@
 # Use scene builder to build random scene
 
 import json
-import os
+from pathlib import Path
 
 from omegaconf import OmegaConf
 
@@ -10,16 +10,17 @@ from clarity.data.scene_builder_cec2 import RoomBuilder, SceneBuilder, set_rando
 
 
 def test_CEC2_scene_builder(regtest):
+    """Regression test for CEC2 scene builder"""
     cfg = OmegaConf.load("tests/test_data/configs/test_CEC2_scene_builder.yaml")
     cfg.path.root = "tests"
     set_random_seed(cfg.random_seed)
     dataset = "train"
-    rb = RoomBuilder()
+    room_builder = RoomBuilder()
 
-    room_file = os.path.join(cfg.path.metadata_dir, f"rooms.{dataset}.json")
-    rb.load(room_file)
-    sb = SceneBuilder(
-        rb=rb,
+    room_file = Path(cfg.path.metadata_dir) / f"rooms.{dataset}.json"
+    room_builder.load(room_file)
+    scene_builder = SceneBuilder(
+        rb=room_builder,
         scene_datasets=cfg.scene_datasets[dataset],
         target=cfg.target,
         interferer=cfg.interferer,
@@ -27,9 +28,9 @@ def test_CEC2_scene_builder(regtest):
         listener=cfg.listener,
         shuffle_rooms=cfg.shuffle_rooms,
     )
-    sb.instantiate_scenes(dataset=dataset)
+    scene_builder.instantiate_scenes(dataset=dataset)
 
-    print(len(sb.scenes))
+    print(len(scene_builder.scenes))
 
     with regtest:
-        print(json.dumps(sb.scenes, indent=4))
+        print(json.dumps(scene_builder.scenes, indent=4))
