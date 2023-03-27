3ae9a4382769e29fbe2078aa0b55a97125886f9a
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-03-27 11:10:08 +0100

    Added place holders for test modules

---
+++ tests/predictor/test_torch_msbg.py
@@ -0,0 +1,5 @@
+"""Module for testing torch_msbg.py"""
+# import numpy as np
+# import pytest
+
+# from clarity.predictor.torch_msbg import generate_key_percent
---
+++ tests/predictor/test_torch_stoi.py
@@ -0,0 +1,5 @@
+"""Module for testing torch_stoi.py"""
+# import numpy as np
+# import pytest
+
+# from clarity.predictor.torch_stoi import meanvar_norm
