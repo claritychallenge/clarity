fe36de6f56efa73e0108a08403e7b7e65afef4b7
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-03-31 19:20:39 +0100

    Fixed remaing pytest warnings. Now runs clean.

--- tests/recipes/cpc2/test_evaluate_cpc2.py
+++ tests/recipes/cpc2/test_evaluate_cpc2.py
@@ -1,6 +1,7 @@
 """Tests for the CPC2 evaluation functions."""
 
 import math
+import warnings
 
 import numpy as np
 import pytest
@@ -30,6 +31,8 @@ def test_rmse_score_ok(x, y, expected):
 def test_rmse_score_error(x, y, expected):
     """Test the function rmse_score for invalid inputs"""
     with pytest.raises(expected):
+        np.seterr(all="ignore")
+        warnings.simplefilter("ignore")  # <--- suppress mean of empty slice warning
         result = rmse_score(np.array(x), np.array(y))
         if np.isnan(result):
             raise ValueError
@@ -99,6 +102,7 @@ def test_std_err_ok(x, y, expected):
 def test_std_err_error(x, y, expected):
     """Test the function std_err for invalid inputs"""
     with pytest.raises(expected):
+        warnings.simplefilter("ignore")
         result = std_err(np.array(x), np.array(y))
         if np.isnan(result):
             raise ValueError
@@ -111,6 +115,7 @@ def test_std_err_error(x, y, expected):
 def test_compute_scores_error(x, y, expected):
     """Test the function compute_scores for invalid inputs"""
     with pytest.raises(expected):
+        warnings.simplefilter("ignore")  # <--- suppress mean of empty slice warning
         compute_scores(np.array(x), np.array(y))
 
 
