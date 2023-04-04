46f10bdc78bbec76c3a56f9c9086ba936e7843db
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-04-04 21:25:27 +0100

    Redesigned test for cec2 additional scene renderer

--- tests/recipes/cec2/baseline/data_generation/test_render_additional_scenes.py
+++ tests/recipes/cec2/baseline/data_generation/test_render_additional_scenes.py
@@ -1,19 +1,24 @@
 # Tests for render_additional_scenes module
 #
-# Very slow test - takes about a minute to render a scene
+import json
+from unittest.mock import MagicMock, patch
 
 import hydra
-import numpy as np
-import pytest
 
-from clarity.evaluator.msbg.msbg_utils import read_signal
-from clarity.recipes.cec2.baseline.data_generation.render_additional_scenes import (
-    render_scenes,
+from clarity.recipes.cec2.baseline.data_generation.render_additional_scenes import run
+
+
+@patch(
+    "clarity.recipes.cec2.baseline.data_generation."
+    "render_additional_scenes.SceneRenderer"
 )
-
-
-def test_render_scenes(tmp_path):
+def test_render_scenes(mock_sr, tmp_path):
     """Test render_scenes function."""
+
+    # Mock out the SceneRenderer as we don't want to actually render
+    # any scenes - it's very slow and tested elsewhere in the unit tests
+    scene_renderer_instance = MagicMock()
+    mock_sr.return_value = scene_renderer_instance
 
     hydra.core.global_hydra.GlobalHydra.instance().clear()
     hydra.initialize(
@@ -33,28 +38,17 @@ def test_render_scenes(tmp_path):
         ],
     )
 
-    render_scenes(hydra_cfg)
+    run(hydra_cfg)
 
-    expected_files = [
-        ("hr", 271413.70622756885),
-        ("interferer_CH0", 1304.3892517089844),
-        ("interferer_CH1", 4294.033477783203),
-        ("interferer_CH2", 4294.033477783203),
-        ("interferer_CH3", 4294.033477783203),
-        ("mix_CH0", 1812.4237365722656),
-        ("mix_CH1", 5105.1387939453125),
-        ("mix_CH2", 5105.1387939453125),
-        ("mix_CH3", 5105.1387939453125),
-        ("target_CH0", 703.9017333984375),
-        ("target_CH1", 1316.4816284179688),
-        ("target_CH2", 1316.4816284179688),
-        ("target_CH3", 1316.4816284179688),
-        ("target_anechoic_CH1", 8006.998596191406),
-    ]
+    # Check the the scene renderer was instantiated
+    assert mock_sr.call_count == 1
+    assert scene_renderer_instance.render_scenes.call_count == 1
 
-    for stem, expected_sum in expected_files:
-        filename = tmp_path / f"S06001_{stem}.wav"
-        assert filename.exists()
-        # Check that the output signal is correct
-        signal = read_signal(filename)
-        assert np.sum(np.abs(signal)) == pytest.approx(expected_sum)
+    # Read the scene data directly
+    with open("tests/test_data/metadata/scenes.test.json", encoding="utf-8") as fp:
+        expected_scene_data = json.load(fp)
+
+    # Check that the scene data was delivered to the SceneRenderer
+    assert (
+        scene_renderer_instance.render_scenes.call_args.args[0] == expected_scene_data
+    )
