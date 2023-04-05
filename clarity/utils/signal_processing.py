d6bb7d73369d3e9e728ec27a24f2555c495888f3
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2023-04-05 17:42:30 +0100

    Add compute RMS function
    Signed-off-by: Gerardo Roa Dabike <gerardo.roa@gmail.com>

--- clarity/utils/signal_processing.py
+++ clarity/utils/signal_processing.py
@@ -1,18 +1,18 @@
+"""Signal processing utilities."""
 from __future__ import annotations
 
 import numpy as np
 
 
-def normalize_signal(signal: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    """Standardize the signal to have zero mean and unit variance.
+def compute_rms(signal: np.ndarray) -> float:
+    """Compute RMS of signal
 
     Args:
-        signal: The signal to be standardized.
+        signal: Signal to compute RMS of.
     Returns:
-        The standardized signal and the reference signal.
+        float: RMS of the signal.
     """
-    ref = signal.mean(0)
-    return (signal - ref.mean()) / ref.std(), ref
+    return np.sqrt(np.mean(np.square(signal)))
 
 
 def denormalize_signals(sources: np.ndarray, ref: np.ndarray) -> np.ndarray:
@@ -25,3 +25,15 @@ def denormalize_signals(sources: np.ndarray, ref: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: Signal rescaled back to its original."""
     return sources * ref.std() + ref.mean()
+
+
+def normalize_signal(signal: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """Standardize the signal to have zero mean and unit variance.
+
+    Args:
+        signal: The signal to be standardized.
+    Returns:
+        The standardized signal and the reference signal.
+    """
+    ref = signal.mean(0)
+    return (signal - ref.mean()) / ref.std(), ref
