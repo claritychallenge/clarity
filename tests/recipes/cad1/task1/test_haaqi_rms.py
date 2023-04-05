14909e5bced318396d88c5c442d7256b613580e6
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2023-04-05 20:12:19 +0100

    add test for haaqi-rms score
    Signed-off-by: Gerardo Roa Dabike <gerardo.roa@gmail.com>

--- tests/recipes/cad1/task1/test_haaqi_rms.py
+++ tests/recipes/cad1/task1/test_haaqi_rms.py
@@ -3,7 +3,11 @@
 import numpy as np
 import pytest
 
-from recipes.cad1.task1.baseline.haaqi_rms import align_signals, find_silence_segments
+from recipes.cad1.task1.baseline.haaqi_rms import (
+    align_signals,
+    compute_haaqi_rms,
+    find_silence_segments,
+)
 
 
 def test_align_signals():
@@ -43,6 +47,37 @@ def test_find_silence_segments():
     silence, non_silence = find_silence_segments(
         reference_signal, sample_rate, silence_length
     )
-    print(silence, non_silence)
     assert silence == [[4000, 7999], [12000, 15999]]
     assert non_silence == [[0, 3999], [8000, 11999]]
+
+
+def test_compute_haaqi_rms():
+    np.random.seed(0)
+    duration = 0.5
+    sample_rate = 1000
+    sig_len = int(duration * sample_rate)
+    reference_signal = np.concatenate(
+        (
+            100 * np.random.random(size=sig_len),
+            np.zeros(sig_len),
+            100 * np.random.random(size=sig_len),
+            np.zeros(sig_len),
+        )
+    )
+    processed_signal = reference_signal.copy()
+    processed_signal[50:] = processed_signal[:-50]
+    processed_signal[0:50] = 0
+
+    processed_signal = align_signals(reference_signal, processed_signal)
+
+    score = compute_haaqi_rms(
+        processed_signal,
+        reference_signal,
+        audiogram=np.array([45, 45, 35, 45, 60, 65]),
+        audiogram_frequencies=np.array([250, 500, 1000, 2000, 4000, 6000]),
+        sample_rate=sample_rate,
+        silence_length=0.1,
+    )
+    assert score == pytest.approx(
+        0.131773752, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+    )
