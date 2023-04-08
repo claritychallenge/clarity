6ee208e1176a57571853f3fa1a8c225a9c84afc5
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-04-08 10:26:38 +0100

    Added stubs for cec1 and cpc1 contributed recipes

---
+++ tests/recipes/cpc1/e032_sheffield/test_transformer_cpc1_decoder.py
@@ -0,0 +1,8 @@
+"""Tests for cpc1 e032_sheffield transformer module."""
+
+import pytest
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_placeholder():
+    """Dummy test to avoid pytest error."""
