4c611f23e325904f3c50f32c53a090bb78621c71
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-04-09 17:33:20 +0100

    Added test for cpc1 e029_sheffield prepare_data

--- tests/recipes/cpc1/e029_sheffield/test_prepare_data.py
+++ tests/recipes/cpc1/e029_sheffield/test_prepare_data.py
@@ -1,8 +1,38 @@
 """Tests for cpc1 e029_sheffield prepare_data module."""
 
-import pytest
+from pathlib import Path
+
+import hydra
+import numpy as np
+
+from recipes.cpc1.e029_sheffield.prepare_data import run
 
 
-@pytest.mark.skip(reason="Not implemented yet")
-def test_placeholder():
-    """Dummy test to avoid pytest error."""
+def test_run(tmp_path):
+    """Test for the run function."""
+    np.random.seed(0)
+
+    hydra.core.global_hydra.GlobalHydra.instance().clear()
+    hydra.initialize(
+        config_path="../../../../recipes/cpc1/e029_sheffield", job_name="test_cpc1_e029"
+    )
+    hydra_cfg = hydra.compose(
+        config_name="config",
+        overrides=[
+            "path.root=tests/test_data/recipes/cpc1/e029_sheffield",
+            f"path.exp_folder={tmp_path}",
+        ],
+    )
+
+    root = Path("tests/test_data/recipes/cpc1/e029_sheffield/")
+    expected_files = [
+        "clarity_CPC1_data_test/clarity_data/HA_outputs/test/"
+        "S08520_L0216_E001_HL-output.wav",
+        "clarity_CPC1_data_train/clarity_data/HA_outputs/train/"
+        "S08510_L0239_E001_HL-output.wav",
+    ]
+    run(hydra_cfg)
+
+    for expected_file in expected_files:
+        assert (root / expected_file).exists()
+        (root / expected_file).unlink()
