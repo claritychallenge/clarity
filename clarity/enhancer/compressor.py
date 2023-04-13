6486866f36ca69ed062883484aac22b21ed70a27
Jon Barker <j.p.barker@sheffield.ac.uk>
2023-04-13 10:34:03 +0100

    Fixed types on nalr and compressor

--- clarity/enhancer/compressor.py
+++ clarity/enhancer/compressor.py
@@ -1,33 +1,35 @@
 """Compressor Class"""
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Final
 
 import numpy as np
+
+EPS: Final = 1e-8
 
 
 class Compressor:
     def __init__(
         self,
-        fs: int = 44100,
-        attack: int = 5,
-        release: int = 20,
-        threshold: float = 1,
+        fs: float = 44100.0,
+        attack: float = 5.0,
+        release: float = 20.0,
+        threshold: float = 1.0,
         attenuation: float = 0.0001,
         rms_buffer_size: float = 0.2,
-        makeup_gain: int = 1,
+        makeup_gain: float = 1.0,
         **_kwargs,
     ) -> None:
         """Instantiate the Compressor Class.
 
         Args:
-            fs (int): (default = 44100)
-            attack (int): (default = 5)
-            release int: (default = 20)
+            fs (float): (default = 44100.0)
+            attack (float): (default = 5.0)
+            release float: (default = 20.0)
             threshold (float): (default = 1.0)
             attenuation (float): (default = 0.0001)
             rms_buffer_size (float): (default = 0.2)
-            makeup_gain (int): (default = 1)
+            makeup_gain (float): (default = 1.0)
         """
         self.fs = fs
         self.rms_buffer_size = rms_buffer_size
@@ -35,7 +37,6 @@ class Compressor:
         self.set_release(release)
         self.threshold = threshold
         self.attenuation = attenuation
-        self.eps = 1e-8
         self.makeup_gain = makeup_gain
 
         # window for computing rms
@@ -51,8 +52,8 @@ class Compressor:
         Returns:
             float: DESCRIPTION
         """
-        t_sec = t_msec / 1000
-        reciprocal_time = 1 / t_sec
+        t_sec = t_msec / 1000.0
+        reciprocal_time = 1.0 / t_sec
         self.attack = reciprocal_time / self.fs
 
     def set_release(self, t_msec: float) -> None:
@@ -64,8 +65,8 @@ class Compressor:
         Returns:
             float: DESCRIPTION
         """
-        t_sec = t_msec / 1000
-        reciprocal_time = 1 / t_sec
+        t_sec = t_msec / 1000.0
+        reciprocal_time = 1.0 / t_sec
         self.release = reciprocal_time / self.fs
 
     def process(self, signal: np.ndarray) -> tuple[np.ndarray, np.ndarray, list[Any]]:
@@ -80,17 +81,17 @@ class Compressor:
         padded_signal = np.concatenate((np.zeros(self.win_len - 1), signal))
         rms = np.sqrt(
             np.convolve(padded_signal**2, self.window, mode="valid") / self.win_len
-            + self.eps
+            + EPS
         )
         comp_ratios: list[float] = []
         curr_comp: float = 1.0
         for rms_i in rms:
             if rms_i > self.threshold:
                 temp_comp = (rms_i * self.attenuation) + (
-                    (1 - self.attenuation) * self.threshold
+                    (1.0 - self.attenuation) * self.threshold
                 )
-                curr_comp = (curr_comp * (1 - self.attack)) + (temp_comp * self.attack)
+                curr_comp = curr_comp * (1.0 - self.attack) + (temp_comp * self.attack)
             else:
-                curr_comp = (1 * self.release) + curr_comp * (1 - self.release)
+                curr_comp = self.release + curr_comp * (1 - self.release)
             comp_ratios.append(curr_comp)
         return (signal * np.array(comp_ratios) * self.makeup_gain), rms, comp_ratios
--- clarity/enhancer/nalr.py
+++ clarity/enhancer/nalr.py
@@ -31,18 +31,19 @@ class NALR:
         self.delay[nfir // 2] = 1.0
 
     def hl_interp(self, hl: np.ndarray, cfs: np.ndarray) -> ndarray:
+        """Interpolate the hearing loss at the audiometric frequencies."""
         try:
-            hl_interpf = scipy.interpolate.interp1d(cfs, hl)
+            hl_interp_fn = scipy.interpolate.interp1d(cfs, hl)
         except ValueError as exception:
             raise ValueError(
                 "Hearing losses (hl) and center frequencies (cfs) don't match!"
             ) from exception
-        return hl_interpf(self.aud)
+        return hl_interp_fn(self.aud)
 
     def build(
         self,
-        hl: np.ndarray,
-        cfs: np.ndarray | None = None,
+        hl: ndarray,
+        cfs: ndarray | None = None,
     ) -> tuple[ndarray, ndarray]:
         """
         Args:
@@ -56,35 +57,36 @@ class NALR:
 
         # Apply interpolation only if cfs is not None
         if cfs is not None:
-            hl = self.hl_interp(np.array(hl), np.array(cfs))
+            hl = self.hl_interp(hl, cfs)
 
-        mloss = np.max(hl)
+        max_loss = np.max(hl)
 
-        if mloss > 0:
+        if max_loss > 0:
             # Compute the NAL-R frequency response at the audiometric frequencies
             bias = np.array([-17, -8, 1, -1, -2, -2])
-            t3 = hl[1] + hl[2] + hl[3]
-            if t3 <= 180:
-                xave = 0.05 * t3
+
+            critical_loss = hl[1] + hl[2] + hl[3]  # <-- loss at 500 Hz to 2 kHz
+            if critical_loss <= 180:
+                x_ave = 0.05 * critical_loss
             else:
-                xave = 9.0 + 0.116 * (t3 - 180)
-            gdB = xave + 0.31 * hl + bias
-            gdB = np.clip(gdB, a_min=0, a_max=None)
+                x_ave = 9.0 + 0.116 * (critical_loss - 180)
+            gain_db = x_ave + 0.31 * hl + bias
+            gain_db = np.clip(gain_db, a_min=0, a_max=None)
 
             # Design the linear-phase FIR filter
-            fv = np.append(
-                np.append(0, self.aud), self.fmax
-            )  # Frequency vector for the interpolation
-            cfreq = (
-                np.linspace(0, self.nfir, self.nfir + 1) / self.nfir
-            )  # Uniform frequency spacing from 0 to 1
-            gdBv = np.append(
-                np.append(gdB[0], gdB), gdB[-1]
-            )  # gdB vector for the interpolation
-            interpf = scipy.interpolate.interp1d(fv, gdBv)
-            gain = interpf(self.fmax * cfreq)
-            glin = np.power(10, gain / 20.0)
-            nalr = firwin2(self.nfir + 1, cfreq, glin)
+
+            # Build the gain interpolation function
+            freq_ext: ndarray = np.concatenate(
+                (np.array([0.0]), self.aud, np.array([self.fmax]))
+            )
+            gain_db_ext = np.concatenate(([gain_db[0]], gain_db, [gain_db[-1]]))
+            interp_fn = scipy.interpolate.interp1d(freq_ext, gain_db_ext)
+
+            # Interpolate gains at uniform frequency spacing from 0 to 1
+            center_freqs = np.linspace(0, self.nfir, self.nfir + 1) / self.nfir
+            interpolated_gain_db = interp_fn(self.fmax * center_freqs)
+            interpolated_gain_linear = np.power(10, interpolated_gain_db / 20.0)
+            nalr = firwin2(self.nfir + 1, center_freqs, interpolated_gain_linear)
         else:
             nalr = self.delay.copy()
         return nalr, self.delay
