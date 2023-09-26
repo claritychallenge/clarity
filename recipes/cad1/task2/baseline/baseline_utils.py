26de49b5f12a9854a35065959c4f150fa96445ea
Gerardo Roa Dabike <gerardo.roa@gmail.com>
2023-09-26 17:39:15 +0100

    cad1 task2 v0.4
    Signed-off-by: Gerardo Roa Dabike <gerardo.roa@gmail.com>

--- recipes/cad1/task2/baseline/baseline_utils.py
+++ recipes/cad1/task2/baseline/baseline_utils.py
@@ -93,15 +93,9 @@ def load_listeners_and_scenes(
         df_scenes = pd.read_json(fp, orient="index")
 
     # Load audiograms and scene data for the corresponding split
-    if config.evaluate.split == "train":
-        listeners = Listener.load_listener_dict(config.path.listeners_train_file)
-        scenes = df_scenes[df_scenes["split"] == "train"].to_dict("index")
-    elif config.evaluate.split == "valid":
-        listeners = Listener.load_listener_dict(config.path.listeners_valid_file)
-        scenes = df_scenes[df_scenes["split"] == "valid"].to_dict("index")
-    elif config.evaluate.split == "test":
-        listeners = Listener.load_listener_dict(config.path.listeners_test_file)
-        scenes = df_scenes[df_scenes["split"] == "test"].to_dict("index")
+    listeners = Listener.load_listener_dict(config.path.listeners_file)
+    if config.evaluate.split in ["train", "valid", "test"]:
+        scenes = df_scenes[df_scenes["split"] == config.evaluate.split].to_dict("index")
     else:
         raise ValueError(f"Unknown split {config.evaluate.split}")
 
