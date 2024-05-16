c4b480f1d91312916111f8e9760f73c6234e96fc
Gerardo Roa <gerardo.roa@gmail.com>
2024-05-16 15:10:55 +0100

    Simulate a constructor overloading depending on the number of crossover frequencies

---
+++ clarity/enhancer/multiband_compressor/crossover/__init__.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from clarity.enhancer.multiband_compressor.crossover.crossover_one import CrossoverOne
+from clarity.enhancer.multiband_compressor.crossover.crossover_two import (
+    CrossoverTwoOrMore,
+)
+
+
+class Crossover:
+    """
+    Emulate a multiple constructors of the Crossover class to facilitate the use.
+    """
+
+    def __init__(self, *args):
+        if isinstance(args[0], float) or isinstance(args[0], int):
+            self.crossover = CrossoverOne(*args)
+        elif isinstance(args[0], list) or isinstance(args[0], np.ndarray):
+            if len(args[0]) == 1:
+                self.crossover = CrossoverOne(*args)
+            else:
+                self.crossover = CrossoverTwoOrMore(*args)
+        else:
+            raise ValueError("Invalid arguments.")
+
+
+__all__ = ["Crossover"]
