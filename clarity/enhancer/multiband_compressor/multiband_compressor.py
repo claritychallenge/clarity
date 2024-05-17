55ce6a89452b8ddd6139f1fafabfee04fb92e588
Gerardo Roa <gerardo.roa@gmail.com>
2024-05-17 11:56:46 +0100

    class to run the multiband compressor

--- clarity/enhancer/multiband_compressor/multiband_compressor.py
+++ clarity/enhancer/multiband_compressor/multiband_compressor.py
@@ -25,7 +25,7 @@ class Compressor:
         ratio: float = 1.0,
         attack: float = 15.0,
         release: float = 100.0,
-        makeUpGain: float = 0.0,
+        gain: float = 0.0,
         sample_rate: float = 44100.0,
     ) -> None:
         if threshold > 0 or threshold < -60:
@@ -36,17 +36,17 @@ class Compressor:
             raise ValueError("Attack must be between 0.1 and 80. ms.")
         if release < 0.1 or release > 1000.0:
             raise ValueError("Release must be between 0.1 and 1000. ms.")
-        if makeUpGain < 0 or makeUpGain > 24:
+        if gain < 0 or gain > 24:
             raise ValueError("Make-up gain must be between 0 and 24 dB.")
 
         self.threshold = threshold
         self.ratio = ratio
-        self.attackTime = attack
+        self.attack = attack
         self.release = release
-        self.makeUpGain = makeUpGain
+        self.gain = gain
         self.sample_rate = sample_rate
 
-    def processBlock(self, input_signal: np.ndarray) -> np.ndarray:
+    def process(self, input_signal: np.ndarray) -> np.ndarray:
         """Process the signal.
         The method processes the input signal and returns the output signal.
 
@@ -56,15 +56,13 @@ class Compressor:
         Returns:
             np.ndarray: The output signal.
         """
-        alphaAttack = np.exp(-1.0 / (0.001 * self.sample_rate * self.attackTime))
+        alphaAttack = np.exp(-1.0 / (0.001 * self.sample_rate * self.attack))
         alphaRelease = np.exp(-1.0 / (0.001 * self.sample_rate * self.release))
 
-        channels = len(input_signal.shape)
-        if channels not in [1, 2]:
-            raise ValueError("Only mono and stereo signals are supported.")
-        if input_signal.shape[0] < input_signal.shape[-1]:
+        if input_signal.shape[0] > input_signal.shape[-1]:
             # Channel First
             input_signal = input_signal.T
+        channels = input_signal.shape[0]
 
         output_signal = np.zeros_like(input_signal)
         yL_prev = np.zeros(input_signal.shape[-1])
@@ -88,7 +86,7 @@ class Compressor:
                 else:
                     y_l = alphaRelease * yL_prev[j] + (1 - alphaRelease) * x_l
 
-                c = 10 ** ((self.makeUpGain - y_l) / 20.0)
+                c = 10 ** ((self.gain - y_l) / 20.0)
                 yL_prev[j] = y_l
 
                 output_signal[j, i] = inputSignal * c
@@ -99,8 +97,7 @@ class Compressor:
 class MultibandCompressor:
     def __init__(
         self,
-        center_frequencies: list | np.ndarray = None,
-        crossover_frequencies: list | np.ndarray = None,
+        center_frequencies: list | np.ndarray,
         order: int = 4,
         sample_rate: float = 44100,
     ) -> None:
@@ -109,33 +106,101 @@ class MultibandCompressor:
 
         Args:
             center_frequencies (list | np.ndarray): The center frequencies of the bands.
-              If center_frequencies are not None, the crossover_frequencies are ignored
-              and computed as center_frequencies[:-2] / sqrt(2).
-            crossover_frequencies (list | np.ndarray): The crossover frequencies.
-              Ignored if center_frequencies are provided.
+              The crossover_frequencies are computed as
+              center_frequencies[:-2] / sqrt(2).
             order (int): The order of the crossover filters.
             sample_rate (float): The sample rate of the signal.
         """
-        if center_frequencies is None and crossover_frequencies is None:
-            raise ValueError(
-                "Either center_frequencies or crossover_frequencies must be provided."
-            )
 
         self.center_frequencies = center_frequencies
-        if center_frequencies is not None:
-            self.xover_freqs = self.compute_crossover_freqs()
-        else:
-            self.xover_freqs = np.array(crossover_frequencies)
+        self.xover_freqs = self.compute_crossover_freqs()
 
         # Compute the crossover filters
         self.crossover = Crossover(self.xover_freqs, order, sample_rate)
+
         self.sample_rate = sample_rate
 
         self.attack = list()
         self.release = list()
-        self.kneewidth = list()
         self.threshold = list()
+        self.ratio = list()
+        self.gain = list()
         self.compressor = list()
+
+        # TODO Create the compressors
+
+    def set_compressors(
+        self,
+        attack: list | float = 15.0,
+        release: list | float = 100.0,
+        threshold: list | float = 0.0,
+        ratio: list | float = 1.0,
+        gain: list | float = 0.0,
+    ) -> None:
+        """Set the compressors parameters.
+
+        Parameters can be a float or a list with the same length as center_frequencies.
+
+        Args:
+            attack (list | float): The attack time in milliseconds.
+            release (list | float): The release time in milliseconds.
+            threshold (list | float): The threshold in dB.
+            ratio (list | float): The ratio.
+            gain (list | float): The make-up gain in dB.
+
+        Raises:
+            ValueError: If either of the parameters are a list with a different length
+                as center_frequencies.
+        """
+
+        num_compressors = len(self.center_frequencies)
+        if isinstance(attack, float):
+            attack = [attack] * num_compressors
+        if isinstance(release, float):
+            release = [release] * num_compressors
+        if isinstance(threshold, float):
+            threshold = [threshold] * num_compressors
+        if isinstance(ratio, float):
+            ratio = [ratio] * num_compressors
+        if isinstance(gain, float):
+            gain = [gain] * num_compressors
+
+        if len(attack) != num_compressors:
+            raise ValueError(
+                "Attack must be a float or have the same "
+                "length as center_frequencies."
+            )
+        if len(release) != num_compressors:
+            raise ValueError(
+                "Release must be a float or have the same "
+                "length as center_frequencies."
+            )
+        if len(threshold) != num_compressors:
+            raise ValueError(
+                "Threshold must be a float or have the same "
+                "length as center_frequencies."
+            )
+        if len(ratio) != num_compressors:
+            raise ValueError(
+                "Ratio must be a float or have the same "
+                "length as center_frequencies."
+            )
+        if len(gain) != num_compressors:
+            raise ValueError(
+                "Gain must be a float or have the same " "length as center_frequencies."
+            )
+
+        self.compressor = [
+            Compressor(
+                attack=attack[i],
+                release=release[i],
+                threshold=threshold[i],
+                ratio=ratio[i],
+                gain=gain[i],
+                sample_rate=self.sample_rate,
+            )
+            for i in range(num_compressors)
+        ]
 
     def compute_crossover_freqs(self) -> list:
         """Compute the crossover frequencies.
@@ -149,15 +214,37 @@ class MultibandCompressor:
             return [self.center_frequencies[0] * np.sqrt(2)]
         return [float(x) * np.sqrt(2) for x in self.center_frequencies[:-1]]
 
-    def __call__(self, signal: np.ndarray) -> np.ndarray:
-        """Compress the signal."""
-        split_signal = self.crossover(signal)
-        compressed_signal = np.zeros_like(signal)
-        for idx, sig in enumerate(split_signal):
-            compressed_band, _, _ = self.compressor[idx].process(sig)
-            compressed_signal += compressed_band
+    def __call__(
+        self, signal: np.ndarray, return_bands: bool = False
+    ) -> np.ndarray | tuple:
+        """Compress the signal.
 
-        return compressed_signal
+        Args:
+            signal (np.ndarray): The input signal.
+            return_bands (bool): If True, return the compressed bands.
+
+        Returns:
+            np.ndarray: The compressed signal.
+            np.ndarray: The compressed bands if return_bands is True.
+
+        Raises:
+            ValueError: If the compressors are not set.
+        """
+
+        if len(self.compressor) == 0:
+            raise ValueError("Compressors not set. Use set_compressors method.")
+
+        split_signal = self.crossover(signal)
+        compressed_signal = np.zeros_like(split_signal)
+
+        for idx, sig in enumerate(split_signal):
+            compressed_signal[idx] = (
+                self.compressor[idx].process(sig[np.newaxis, :]).squeeze(0)
+            )
+
+        if return_bands:
+            return np.sum(compressed_signal, axis=0), compressed_signal
+        return np.sum(compressed_signal, axis=0)
 
     def __str__(self):
         out_text = "Multiband Compressor Summary:\n"
@@ -167,5 +254,43 @@ class MultibandCompressor:
 
 
 if __name__ == "__main__":
+    import librosa
+    import matplotlib.pyplot as plt
+    from scipy.io import wavfile
+
+    signal, sr = librosa.load("bwv608.mp3", sr=None, duration=10, mono=False)
+    signal = signal[0, :]
+
     mbc = MultibandCompressor(center_frequencies=[250, 500, 1000, 2000, 4000, 8000])
     print(mbc.xover_freqs)
+    print(mbc)
+
+    HL = np.array([20, 20, 30, 40, 50, 60])
+    gains = np.maximum((HL - 20) / 3, 0)
+    cratio = [1.32, 1.32, 1.67, 1.74, 1.45, 1.45]
+    cattack_ms = [11, 11, 14, 13, 11, 11]
+    crelease_ms = [80, 80, 80, 80, 100, 100]
+    cthreshold = [-30, -30, -30, -30, -30, -30]
+
+    mbc.set_compressors(
+        attack=cattack_ms,
+        release=crelease_ms,
+        threshold=cthreshold,
+        ratio=cratio,
+        gain=gains,
+    )
+    compressed_signal, compressed_bands = mbc(signal, return_bands=True)
+    fig, axes = plt.subplots(8, 1, figsize=(10, 10))
+    axes[0].specgram(signal, Fs=sr, NFFT=512, noverlap=256)
+    axes[0].set_title("Original Signal")
+    wavfile.write("original_signal.wav", sr, signal)
+    for idx, band in enumerate(compressed_bands):
+        axes[idx + 1].specgram(band, Fs=sr, NFFT=512, noverlap=256)
+        axes[idx + 1].set_title(f"Band {idx + 1}")
+        wavfile.write(f"band_{idx}.wav", sr, band)
+
+    axes[-1].specgram(compressed_signal, Fs=sr, NFFT=512, noverlap=256)
+    axes[-1].set_title("Compressed Signal")
+    wavfile.write("compressed_signal.wav", sr, compressed_signal)
+    plt.tight_layout()
+    plt.show()
