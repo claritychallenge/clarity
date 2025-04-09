c262540138a2831e18505f063451304d5def75e8
Gerardo Roa <gerardo.roa@gmail.com>
2025-04-09 12:07:23 +0100

    Add smear to test and split long tests
    Signed-off-by: Gerardo Roa <gerardo.roa@gmail.com>

--- tests/evaluator/msbg/test_msbg.py
+++ tests/evaluator/msbg/test_msbg.py
@@ -15,15 +15,16 @@ def test_ear():
     assert ear.ahr == 20
 
 
-def test_set_audiogram():
-    """Test Ear.set_audiogram"""
+@pytest.mark.parametrize("apply_smear", [True, False])
+def test_set_audiogram(apply_smear):
+    """Test Ear.set_audiogram. including applying or not smearer"""
     ear = Ear()
-    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE)
+    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE, apply_smear=apply_smear)
     assert ear.cochlea is not None
 
     # Bad audiogram - will log a warning
     audiogram_bad = Audiogram(levels=np.array([150]), frequencies=np.array([4000]))
-    ear.set_audiogram(audiogram=audiogram_bad)
+    ear.set_audiogram(audiogram=audiogram_bad, apply_smear=apply_smear)
     assert ear.cochlea is not None
 
 
@@ -85,7 +86,11 @@ def test_make_calibration_signal(n_channels):
     )
 
 
-def test_ear_process():
+@pytest.mark.parametrize(
+    "apply_smear, out_size, signal_out",
+    [(True, 704, 0.5448164192836198), (False, 500, 0.34154423638012266)],
+)
+def test_ear_process_1(apply_smear, out_size, signal_out):
     """Test ear.process"""
     np.random.seed(0)
 
@@ -100,41 +105,108 @@ def test_ear_process():
         0.3250822358794586, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
     )
 
-    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE)
+    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE, apply_smear=apply_smear)
 
     processed_signals = ear.process(signal)
     assert len(processed_signals) == 1
-    assert processed_signals[0].shape == (704,)  # longer than input due to smearing
+    assert processed_signals[0].shape == (
+        out_size,
+    )  # longer than input due to smearing
     assert np.sum(np.abs(processed_signals[0])) == pytest.approx(
-        0.5448164192836198, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+        signal_out, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
     )
 
+
+@pytest.mark.parametrize(
+    "apply_smear, out_size, out_1, out_2",
+    [
+        (True, 704, 0.4983800098691009, 0.514370533984153),
+        (False, 500, 0.3230867040311348, 0.33242205144806053),
+    ],
+)
+def test_ear_process_2(
+    apply_smear,
+    out_size,
+    out_1,
+    out_2,
+):
+    """Test ear.process"""
+    np.random.seed(0)
+    signal = np.random.rand(500)  # Generate to replicate previous tests
     signal2 = np.random.rand(500, 2)
+
+    ear = Ear()
+    # Can process with audiogram
+    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE, apply_smear=apply_smear)
+
     processed_signals = ear.process(signal2)
     assert len(processed_signals) == 2
-    assert processed_signals[0].shape == (704,)
-    assert processed_signals[1].shape == (704,)
+    assert processed_signals[0].shape == (out_size,)
+    assert processed_signals[1].shape == (out_size,)
     assert np.sum(np.abs(processed_signals[0])) == pytest.approx(
-        0.4983800098691009, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+        out_1, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
     )
     assert np.sum(np.abs(processed_signals[1])) == pytest.approx(
-        0.514370533984153, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+        out_2, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
     )
 
-    signal1 = np.random.rand(500, 1)
-    processed_signals = ear.process(signal1)
-    assert len(processed_signals) == 1
-    assert processed_signals[0].shape == (704,)
-    assert np.sum(np.abs(processed_signals[0])) == pytest.approx(
-        0.5840767608116147, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
-    )
+
+@pytest.mark.parametrize(
+    "apply_smear, out_size, out",
+    [
+        (True, 704, 0.5840767608116147),
+        (False, 500, 0.3701162356566565),
+    ],
+)
+def test_ear_process_3(apply_smear, out_size, out):
+    np.random.seed(0)
 
     signal = np.random.rand(500)
+    signal2 = np.random.rand(500, 2)
+    signal1 = np.random.rand(500, 1)
+
+    ear = Ear()
+    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE, apply_smear=apply_smear)
+
+    processed_signals = ear.process(signal1)
+    assert len(processed_signals) == 1
+    assert processed_signals[0].shape == (out_size,)
+    assert np.sum(np.abs(processed_signals[0])) == pytest.approx(
+        out, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+    )
+
+
+@pytest.mark.parametrize(
+    "apply_smear,  out, out_size",
+    [
+        (True, 48480.61370282146, 119808),
+        (False, 58671.800576049194, 119570),
+    ],
+)
+def test_ear_process(
+    apply_smear,
+    out,
+    out_size,
+):
+    """Test ear.process"""
+    np.random.seed(0)
+
+    signal = np.random.rand(500)
+    signal2 = np.random.rand(500, 2)
+    signal1 = np.random.rand(500, 1)
+    signal = np.random.rand(500)
+
+    ear = Ear()
+    # Can process with audiogram
+    processed_signals = ear.process(signal)
+
+    ear.set_audiogram(audiogram=AUDIOGRAM_MODERATE_SEVERE, apply_smear=apply_smear)
+
     processed_signals = ear.process(signal, add_calibration=True)
     assert len(processed_signals) == 1
-    assert processed_signals[0].shape == (119808,)
+    assert processed_signals[0].shape == (out_size,)
     assert np.sum(np.abs(processed_signals[0])) == pytest.approx(
-        48480.61370282146, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
+        out, rel=pytest.rel_tolerance, abs=pytest.abs_tolerance
     )
 
 
