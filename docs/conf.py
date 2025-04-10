28f5230968fa99e78e4c998d81d1b21e77f85c6c
Gerardo Roa <gerardo.roa@gmail.com>
2025-04-10 14:26:47 +0100

    read the doc theme
    Signed-off-by: Gerardo Roa <gerardo.roa@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,32 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pyClarity'
-copyright = '2025, pyClarity authors'
-author = 'pyClarity authors'
-release = 'v0.6.3'
+project = "pyClarity"
+copyright = "2025, pyClarity authors"
+author = "pyClarity authors"
+release = "v0.6.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+html_theme_options = {
+    "navigation_with_keys": True,  # This enables keyboard navigation
+}
+
+html_css_files = [
+    "custom.css",  # Link to your custom CSS file
+]
+
+html_logo = "images/earfinal_clarity_customColour.png"
